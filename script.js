6896e932871cf20d5609e8ccd79740343a6175fb
Jake Walsh <jkwalsh127@gmail.com>
2022-04-07 01:12:15 -0700

    clone event listeners for each save button, saving the form input to local storage

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ function renderTasks() {
             hourlyTaskArray[i].setAttribute("class", "custom-red");
         } else {
             hourlyTaskArray[i].setAttribute("class", "custom-gray");
+            console.log("custom gray");
         }
     };
 }
@@ -57,6 +58,14 @@ renderTasks();
 
 
 var btn9 = document.querySelector("#btn9");
+var btn10 = document.querySelector("#btn10");
+var btn11 = document.querySelector("#btn11");
+var btn12 = document.querySelector("#btn12");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btn3 = document.querySelector("#btn3");
+var btn4 = document.querySelector("#btn4");
+var btn5 = document.querySelector("#btn5");
 
 btn9.addEventListener("click", function(event) {
     var element = event.target;
@@ -66,88 +75,94 @@ btn9.addEventListener("click", function(event) {
         var stateParse = parseInt(state);
         i = stateParse;
         var newTask = hourlyTaskArray[i].value;
-        localStorage.setItem("tasks", JSON.stringify(newTask));
+        localStorage.setItem("task9AM", JSON.stringify(newTask));
     }
 });
+btn10.addEventListener("click", function() {
+    var element = event.target;
 
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[1].value;
+        localStorage.setItem("task10AM", JSON.stringify(newTask));
+    }
+});
+btn11.addEventListener("click", function() {
+    var element = event.target;
 
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[2].value;
+        localStorage.setItem("task11AM", JSON.stringify(newTask));
+    }
+});
+btn12.addEventListener("click", function() {
+    var element = event.target;
 
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[3].value;
+        localStorage.setItem("task12AM", JSON.stringify(newTask));
+    }
+});
+btn1.addEventListener("click", function() {
+    var element = event.target;
 
-// var todoInput = document.querySelector("#todo-text");
-// var todoForm = document.querySelector("#todo-form");
-// var todoList = document.querySelector("#todo-list");
-// var todoCountSpan = document.querySelector("#todo-count");
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[4].value;
+        localStorage.setItem("task1PM", JSON.stringify(newTask));
+    }
+});
+btn2.addEventListener("click", function() {
+    var element = event.target;
 
-// var saveBtn = document.querySelector(".btn");
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[5].value;
+        localStorage.setItem("task2PM", JSON.stringify(newTask));
+    }
+});
+btn3.addEventListener("click", function() {
+    var element = event.target;
 
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[6].value;
+        localStorage.setItem("task3PM", JSON.stringify(newTask));
+    }
+});
+btn4.addEventListener("click", function() {
+    var element = event.target;
 
-// var todos = [];
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[7].value;
+        localStorage.setItem("task4PM", JSON.stringify(newTask));
+    }
+});
+btn5.addEventListener("click", function() {
+    var element = event.target;
 
-
-// // This function is being called below and will run when the page loads.
-// //We've stored the to-do items, but we also need to be able to retrieve the data from storage. We will do this inside the init() function.
-// //init() is being invoked in script.js, so it will run every time the page loads.
-// //Inside of init(), we use getItem() and convert the data from a string to an array using JSON.parse(). To use the array in the code, we store it in a variable
-
-// function init() {
-//   // Get stored todos from localStorage
-//   var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-//   // If todos were retrieved from localStorage, update the todos array to it
-//   if (storedTodos !== null) {
-//     todos = storedTodos;
-//   }
-
-//   // This is a helper function that will render todos to the DOM
-//   renderTodos();
-// }
-
-// //The storeTodos function stores an array using localStorage. First we stringify it using JSON.stringify(), then we set it to storage using setItem().
-// //We need to save to-do items to localStorage in a few places, so storing this action in the function storeTodos cleans up the code! 
-// function storeTodos() {
-//   // Stringify and set key in localStorage to todos array
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-
-
-// // Add submit event to form
-// //We add an event listener to the to-do form so that when a user enters a to-do item, the data is stored on submit. The to-dos are stored and rendered using two helper functions, storeTodos() and renderTodos()
-// saveBtn.addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   var taskInput = hourlyTaskArray[i].trim();
-
-//   // Return from function early if submitted taskInputt is blank
-//   if (taskInputt === "") {
-//     return;
-//   } 
-
-//   // Store updated todos in localStorage, re-render the list
-//   storeTodos();
-//   renderTodos();
-// });
-
-// // Add click event to todoList element
-// //Finally, we need to add an event listener to the completed button so that a user can remove to-do items. We do this by using the "data-index" of the button's parent element to remove the correct to-do item from the todos array.
-// //We call the storeTodos() and renderTodos() functions when the complete button is clicked, to store and render the updated list
-// todoList.addEventListener("click", function(event) {
-//   var element = event.target;
-//   console.log(element);
-//   console.log("I was clicked");
-
-//   // Checks if element is a button
-//   if (element.matches("button") === true) {
-//     // Get its data-index value and remove the todo element from the list
-//     var index = element.parentElement.getAttribute("data-index");
-//         // ["take a walk","code","study"]
-//     todos.splice(index, 1);
-
-//     // Store updated todos in localStorage, re-render the list
-//     storeTodos();
-//     renderTodos();
-//   }
-// });
-
-// // Calls init to retrieve data and render it to the page on load
-// init()
+    if (element.matches(".btn")) {
+        var state = element.getAttribute("data-number");
+        var stateParse = parseInt(state);
+        i = stateParse;
+        var newTask = hourlyTaskArray[8].value;
+        localStorage.setItem("task5PM", JSON.stringify(newTask));
+    }
+});
