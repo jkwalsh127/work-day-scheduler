47acd0b72574efc0d44005766754955f2c07637b
Jake Walsh <jkwalsh127@gmail.com>
2022-04-06 19:18:17 -0700

    declare objects to apply to each hourly task

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $(document).ready(function () {
 
 });
 
+// Object hourlyTask with three properties
+//We declare a hourlyTask object using object literal notation. We give the object three properties, as follows:
+var hourlyTask= {
+    //We use key-value pairs to define each property, like in the following example:
+    time: 9,
+    task: "aisudhfkasfdnk",
+    button: 9,
+  };
+  
+  // This logs the entire object
+  //To access the whole object, we use the object's name, as follows:
+    console.log(hourlyTask);
+  
+  // Note that in bracket notation, the key is inside brackets and surrounded by quotes
+  //We can also use bracket notation to access a value, as follows:
+    console.log("   " + hourlyTask["time"] + "  " + hourlyTask["task"] + "  ");
+  
